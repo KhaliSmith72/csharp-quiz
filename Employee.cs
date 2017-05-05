78eaa76e91a760682b8b8a93e248337b919f6463
Khali Smith <Khlsmith72@Gmail.com>
2017-05-05 17:07:07 -0400

    Updates

---
+++ Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyTest
+{
+	class Employee
+	{
+		public static CloneEmployee(object employee)
+		{
+			var p = employee;
+
+			return p;
+		}
+
+		
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string HireDate { get; set; }
+		public string Email { get; set; }
+		public static NumofDays(Employee)
+		{
+			
+
+
+		}
+		public override string ToString()
+		{
+			return $"FirstName: {FirstName}, LastName: {LastName}, HireDate: {HireDate}, Email: {Email}.";
+		}
+	}	
+}
